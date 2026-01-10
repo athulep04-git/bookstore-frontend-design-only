20ac1c1b1b3a05d96dd0499c47f80a75042c7aef
athulep04-git <athulep04@gmail.com>
2026-01-10 11:37:03 +0530

    api called

--- src/admin/pages/AdminBooks.jsx
+++ src/admin/pages/AdminBooks.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import AdminHeader from '../components/AdminHeader'
 import AdminSideBar from '../components/AdminSideBar'
-import { Button, Card } from "flowbite-react";
+import { Card } from "flowbite-react";
 import { TabItem, Tabs } from "flowbite-react";
-import { HiAdjustments, HiClipboardList, HiUserCircle } from "react-icons/hi";
+import { HiUserCircle } from "react-icons/hi";
 import { MdDashboard } from "react-icons/md";
-import { userlistAPI } from '../../services/allAPIs';
+import { booklistAPI, userlistAPI } from '../../services/allAPIs';
 
 function AdminBooks() {
   const [token, setToken] = useState("");
   const [users,setUsers]=useState([])
+  const[allBooks,setAllBooks]=useState([])
   useEffect(() => {
       setToken(sessionStorage.getItem("token"));
     }, []);
@@ -27,8 +28,30 @@ function AdminBooks() {
         
       }
   }
+  const getAllBooks=async()=>{
+        
+        try{
+          const reqheader={
+            Authorization:`Bearer ${token}`
+          };
+          console.log(reqheader);
+          
+          const  booklist= await booklistAPI(reqheader)
+          console.log(booklist);
+          setAllBooks(booklist.data)
+          
+        }
+        catch(err){
+          console.log("error"+err);
+          
+        }
+      }
+      console.log(allBooks);
+      
+  
   useEffect(()=>{
-      userlist()
+    getAllBooks()
+    userlist()
   },[token])
 
   console.log(users);
@@ -48,22 +71,31 @@ function AdminBooks() {
                 <div className="ps-10">
                   <Tabs aria-label="Full width tabs" variant="fullWidth">
                     <TabItem active title="BookList" icon={HiUserCircle}>
-
-                      <div class="flex flex-wrap ">
-                        <div class="basis-2xs">
-                          <Card
-                            className="w-50 m-5 !bg-amber-50"     >
-                            <img src="https://cdn.fcglcdn.com/brainbees/images/products/583x720/8801697a.webp" width={'150px'} alt="" />
-                            <h5 className=" font-bold tracking-tight text-gray-900">
-                              The Lost World Illustrated Abridged Classics - English
-                              236 MRP: 295 20% OFF
-                            </h5>
-
-                          </Card>
-                        </div>
+                      <div className="flex flex-wrap">
+                        {
+                          allBooks.length === 0 ? (
+                            <p className="text-center w-full">No books found</p>
+                          ) : (
+                            allBooks.map(book => (
+                              <div key={book._id} className="basis-1/4">
+                                <Card className="w-50 m-5 !bg-amber-50">
+                                  <img src={book.imageUrl} width="150" alt={book.title} />
+                                  <h5 className="font-bold text-gray-900">
+                                    {book.title}
+                                  </h5>
+                                  <p>
+                                    MRP: {book.price}  
+                                    <br />
+                                    Offer: {book.dprice}
+                                  </p>
+                                </Card>
+                              </div>
+                            ))
+                          )
+                        }
                       </div>
-
                     </TabItem>
+
                     <TabItem title="Users" icon={MdDashboard}>
                       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
 
@@ -73,7 +105,7 @@ function AdminBooks() {
                           :
                           users.map(item=>(
                           <div className="basis-2xs">
-                          <Card
+                          <Card  key={item._id}
                             className="flex justify-evenly w-80 m-5 !bg-amber-50"     >
                             <div className='basis-4xs'>
                               <p>ID :{item._id}</p>
